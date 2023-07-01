485cc29fec5869df99d2000dfa6dc2c42f175d5f
ivanG <ivangg02@gmail.com>
2023-07-01 14:57:30 +0200

    docente -> nethermir

--- node-server/src/database.js
+++ node-server/src/database.js
@@ -31,36 +31,36 @@ const queryToDB = (sql, params) => {
 }
 
 const getGroups = (res) => {    
-    sql = `SELECT * FROM docente.groups`
+    sql = `SELECT * FROM nethermir.groups`
     queryToDB(sql).then(x =>{feedback_fetch(JSON.stringify(x), res)})
 }
 
 const eliminateGroup = (req, res) => {
     id = req.query['id'];
-    sql = `DELETE FROM docente.groups WHERE idgroup=(?)`
+    sql = `DELETE FROM nethermir.groups WHERE idgroup=(?)`
     queryToDB(sql, [id]).then(x =>{feedback_fetch("Y", res)})
 }
 
 const getEmails = (res) => {    
-    sql = `SELECT * FROM docente.emails`
+    sql = `SELECT * FROM nethermir.emails`
     queryToDB(sql).then(x =>{feedback_fetch(JSON.stringify(x), res)})
 }
 
 const eliminateEmail = (req, res) => {
     id = req.query['id'];
-    sql = `DELETE FROM docente.emails WHERE email_id=(?)`
+    sql = `DELETE FROM nethermir.emails WHERE email_id=(?)`
     queryToDB(sql, [id]).then(x =>{feedback_fetch("Y", res)})
 }
 
 const getSubjects = (res) => {  
-    sql = `SELECT * FROM docente.subjects`
+    sql = `SELECT * FROM nethermir.subjects`
     queryToDB(sql).then(x =>{feedback_fetch(JSON.stringify(x), res)})
 }
 
 const addSubject = async (req,res) => {
     console.log("addSubject", req.query)
     id = req.query['id'];
-    sql = `INSERT INTO docente.subjects (idsubject, subject_name) VALUES (?, ?)` 
+    sql = `INSERT INTO nethermir.subjects (idsubject, subject_name) VALUES (?, ?)` 
     // id.slice(0,-(id.split('-').pop().length + 1)) This monstrocity gets everything before last dash == subject_name 
     // Ex: 2022-1-FX-1 -> 2022-1-FX        2023-2-STDW-10 -> 2023-2-STDW 
     await queryToDB(sql, [id.split('-').pop(), id.slice(0,-(id.split('-').pop().length + 1))]).catch(x=>console.log(x))
@@ -69,19 +69,19 @@ const addSubject = async (req,res) => {
 const eliminateSubject = (req, res) => {
     console.log("eliminateSubject")
     id = req.query['id'];
-    sql = `DELETE FROM docente.subjects WHERE idsubject=(?)`
+    sql = `DELETE FROM nethermir.subjects WHERE idsubject=(?)`
     queryToDB(sql, [parseInt(id)]).then(x =>{console.log(x);feedback_fetch("Y", res)})
 }
 const activateSubject = (req,res) => {
     console.log("activateSubject", req.query)
     id = req.query['id'];
-    sql = `UPDATE docente.subjects SET active=(active+1)%2 WHERE idsubject=(?)` 
+    sql = `UPDATE nethermir.subjects SET active=(active+1)%2 WHERE idsubject=(?)` 
     queryToDB(sql, [id]).then(x =>{console.log(x);feedback_fetch("Y", res)})
 }
 
 const activateGroup = (id) => {
     console.log("activateGroup")
-    sql = `UPDATE docente.groups SET active=(active+1)%2 WHERE idgroup=(?)` 
+    sql = `UPDATE nethermir.groups SET active=(active+1)%2 WHERE idgroup=(?)` 
     queryToDB(sql, [id]).then(x =>{console.log(x)})
 }
 
@@ -93,7 +93,7 @@ const authenticate = async(req, res) => {
         if (user.startsWith(process.env.ROOT_USER) && pass == process.env.ROOT_PASS){
             resolve("root")
         }else{
-        sql = `SELECT idgroup, active, p_hash FROM docente.groups WHERE name=?`            
+        sql = `SELECT idgroup, active, p_hash FROM nethermir.groups WHERE name=?`            
         queryToDB(sql, [user]).then(async x =>{
             if (x.length > 0){
                 pass_hash = x[0].p_hash
@@ -130,15 +130,15 @@ const registerGroup = async (req, res) => {
         return 0
     }
     let promises = [];
-    sql = `INSERT INTO docente.groups (idgroup, name, p_hash) VALUES (?, ?, ?)`                       
+    sql = `INSERT INTO nethermir.groups (idgroup, name, p_hash) VALUES (?, ?, ?)`                       
     promises.push(queryToDB(sql, [idgroup, nameGroup, p_hash])
             .then(console.log("Group Registrat"))
-            .catch(x=>feedback_fetch("Error mySQL docente.groups: " + x, res)))
-    sql = `INSERT INTO docente.emails (email, group_name) VALUES (?, ?) `
+            .catch(x=>feedback_fetch("Error mySQL nethermir.groups: " + x, res)))
+    sql = `INSERT INTO nethermir.emails (email, group_name) VALUES (?, ?) `
     
     emails.forEach(email => promises.push(queryToDB(sql, [email, nameGroup])
         .then(console.log("Email Registrat"))
-        .catch(x=>feedback_fetch("Error mySQL docente.groups: " + x, res)))) 
+        .catch(x=>feedback_fetch("Error mySQL nethermir.groups: " + x, res)))) 
 
     Promise.all(promises).then(async () =>{
         sendPasswordEmail(emails, nameGroup, idgroup, password)
@@ -148,10 +148,10 @@ const registerGroup = async (req, res) => {
 
 function  generateGroup (user) {
     return new Promise(async (resolve, reject) => {
-        sql = `SELECT idsubject FROM docente.subjects WHERE subject_name = (?)` 
+        sql = `SELECT idsubject FROM nethermir.subjects WHERE subject_name = (?)` 
         idsubject = await queryToDB(sql, [user])
         idsubject = idsubject[0]['idsubject']
-        sql = `SELECT MAX(idgroup) as idgroup_max FROM docente.groups WHERE (idgroup - (idgroup MOD 100)) /100 = (?) `                
+        sql = `SELECT MAX(idgroup) as idgroup_max FROM nethermir.groups WHERE (idgroup - (idgroup MOD 100)) /100 = (?) `                
         idgroups = await queryToDB(sql, [idsubject])
         idgroups = idgroups[0]['idgroup_max']
         if (idgroups != null){idgroup = idgroups+1;}
@@ -175,7 +175,7 @@ const checkEmails = (emails) =>{
         elements = 0
         emails.forEach(async email=>{
             if (!email.endsWith('@uab.cat')) {resolve("Doesn't end in @uab.cat")}
-            sql = `SELECT email FROM docente.emails WHERE email=?`
+            sql = `SELECT email FROM nethermir.emails WHERE email=?`
             queryToDB(sql, [email]).then(x =>{ 
                 if (x.length != 0){resolve(`Email ${email} already in database`)}
                 if (elements == emails.length-1) {resolve("Correct")}
@@ -187,12 +187,12 @@ const checkEmails = (emails) =>{
 
 const restartDatabase = async() =>{
     return new Promise(async (resolve, reject) => {
-        sql = `DROP SCHEMA IF EXISTS docente;       
+        sql = `DROP SCHEMA IF EXISTS nethermir;       
         ` 
         await queryToDB(sql)
-        sql = `CREATE SCHEMA docente; `  
+        sql = `CREATE SCHEMA nethermir; `  
         await queryToDB(sql)
-        sql = `CREATE TABLE docente.groups (
+        sql = `CREATE TABLE nethermir.groups (
             idgroup INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(45) NOT NULL,
             p_hash VARCHAR(60) NULL,
@@ -202,7 +202,7 @@ const restartDatabase = async() =>{
             UNIQUE INDEX name_UNIQUE (name ASC) VISIBLE,
             UNIQUE INDEX p_hash_UNIQUE (p_hash ASC) VISIBLE);` 
         await queryToDB(sql)  
-        sql = `CREATE TABLE docente.emails (
+        sql = `CREATE TABLE nethermir.emails (
             email_id INT NOT NULL AUTO_INCREMENT,
             email VARCHAR(45) NOT NULL,
             group_name VARCHAR(45) NOT NULL,
@@ -210,11 +210,11 @@ const restartDatabase = async() =>{
             INDEX group_name_idx (group_name ASC) VISIBLE,
             CONSTRAINT group_name
               FOREIGN KEY (group_name)
-              REFERENCES docente.groups (name)
+              REFERENCES nethermir.groups (name)
               ON DELETE CASCADE
               ON UPDATE CASCADE);`
         await queryToDB(sql)      
-        sql = `CREATE TABLE docente.subjects (
+        sql = `CREATE TABLE nethermir.subjects (
             idsubject INT NOT NULL,
             subject_name VARCHAR(45) NULL,
             active TINYINT NULL DEFAULT 0,
