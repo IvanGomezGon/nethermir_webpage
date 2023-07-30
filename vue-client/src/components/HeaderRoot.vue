eace5b939e61455912ec77fc4377485b39bbe9e7
ivanG <ivangg02@gmail.com>
2023-07-30 21:04:43 +0200

    header tabs style on active

--- vue-client/src/components/HeaderRoot.vue
+++ vue-client/src/components/HeaderRoot.vue
@@ -3,10 +3,10 @@
     <div class="header">
         <img src="../assets/logo_uab.png" class="logo" @click="this.$router.push('/')">
         <h3 class="header_element title">PANELL DE CONTROL ROOT</h3>
-        <a href="#" class="push header_element" @click="switchTab('ProxmoxInfo')">PROXMOX VMs</a>
-        <a href="#" class="header_element" @click="switchTab('groupsInfo')">GRUPS</a>
-        <a href="#" class="header_element" @click="switchTab('emailsInfo')">USUARIS</a>
-        <a href="#" class="header_element" @click="switchTab('subjectsInfo')">ASSIGNATURES</a>
+        <a href="#" :style="getStyle('proxmox')" class="push header_element" @click="switchTab('ProxmoxInfo'); proxmox = !proxmox">PROXMOX VMs</a>
+        <a href="#" :style="getStyle('grups')" class="header_element" @click="switchTab('groupsInfo'); grups = !grups" >GRUPS</a>
+        <a href="#" :style="getStyle('usuaris')" class="header_element" @click="switchTab('emailsInfo'); usuaris = !usuaris">USUARIS</a>
+        <a href="#" :style="getStyle('assignatures')" class="header_element" @click="switchTab('subjectsInfo'); assignatures = !assignatures">ASSIGNATURES</a>
         <button class="logout" @click="this.$router.push('/restartDatabase')">REINICIAR BASE DE DADES</button>
         <button class="logout" @click="eliminateCookie()">SORTIR</button>
     </div>
@@ -16,7 +16,24 @@
   export default {
     name: 'HeaderRoot',
     props:['modelValue'],
+    data: function() {
+        return{
+            proxmox: false,
+            grups: false,
+            usuaris: false,
+            assignatures: false,
+        }
+    },
     methods:{
+        getStyle(tab){
+            if ((this.proxmox && tab == 'proxmox') || 
+                (this.grups && tab == 'grups') || 
+                (this.usuaris && tab == 'usuaris') || 
+                (this.assignatures && tab == 'assignatures')){
+                    return 'border-bottom: 2px solid rgb(32, 102, 0);'
+                }
+            return '';
+        },
         switchTab(tab){
             this.$emit('update:modelValue',  tab == this.modelValue ? '' : tab)
         },
