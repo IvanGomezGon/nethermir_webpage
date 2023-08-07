3f144a184b2d306bbddbc384f6a1804981497a1b
ivanG <ivangg02@gmail.com>
2023-08-07 02:44:07 +0200

    routeros new vlan first attempt, not tested yet

--- node-server/src/routeros.js
+++ node-server/src/routeros.js
@@ -7,14 +7,109 @@ const conn = new RosApi({
     user: process.env.ROUTEROS_USER,
     password: process.env.ROUTEROS_PASSWORD,
 });
-conn.connect()
+
+
+
+const newVLAN = (groupName, wgGroupName, wgGroupPublicKey, port_udp, vlanGroup) => {
+    conn.connect()
     .then(() => {
-        // Connection successful
-        // Let's add an IP address to ether2
-        conn.write('/ip/address/add', [
-            '=interface=ether2',
-            '=address=192.168.90.1',
+        conn.write('/interface/list/add', [
+            `=name=${groupName}`,
         ])
-        logger.info("Done")
+            .then((data) =>{
+                logger.info(`interface list name added, ${data}`)
+                return conn.write('/interface/wireguard/add', [
+                    `=comment=${groupName}`,
+                    `=listen-port=${port_udp}`,
+                    `=mtu=1420`,
+                    `=name=${wgGroupName}`,
+                ])
+            })
+ 
+            .then((data) =>{
+                logger.info(`interface wireguard added, ${data}`)
+                return conn.write('/interface/wireguard/peers/add', [
+                    `=allowed-address=10.0.1.0/30,10.1.1.0/30`,
+                    `=comment=${groupName}`,
+                    `=interface=wireguard_${wgGroupName}`,
+                    `=public-key=${wgGroupPublicKey}`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`interface wireguard peers added, ${data}`)
+                return conn.write('/interface/vlan/add', [
+                    `=comment=${groupName}`,
+                    `=interface=ether3`,
+                    `=name=vlan_${vlanGroup}`,
+                    `=vlan-id=${vlanGroup}`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`interface vlan added, ${data}`)
+                return conn.write('/ip/vrf/add', [
+                    `=interfaces=${groupName}`,
+                    `=name=vrf_${groupName}`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`ip vrf added, ${data}`)
+                return conn.write('/interface/list/member/add', [
+                    `=interface=vlan_${vlanGroup}`,
+                    `=list=${groupName}`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`interface list member vlan added, ${data}`)
+                return conn.write('/interface/list/member/add', [
+                    `=interface=wireguard_${wgGroupName}`,
+                    `=list=${groupName}`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`interface list member wireguard added, ${data}`)
+                return conn.write('/ip/address/add', [
+                    `=address=10.0.1.1/30`,
+                    `=comment=vlan_${vlanGroup}`,
+                    `=interface=vlan_${vlanGroup}`,
+                    `=network=10.0.1.0`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`ip address vlan added, ${data}`)
+                return conn.write('/ip/address/add', [
+                    `=address=10.1.1.1/30`,
+                    `=comment=wireguard_${wgGroupName}`,
+                    `=interface=wireguard_${wgGroupName}`,
+                    `=network=10.1.1.0`,
+                ])
+            })
+
+            .then((data) =>{
+                logger.info(`ip address wireguard added, ${data}`)
+                return conn.write('/ip/firewall/filter/add', [
+                    `=action=accept`,
+                    `=chain=forward`,
+                    `=comment="defconf: accept from ${groupName} to ${groupName}"`,
+                    `=in-interface-list=${groupName}`,
+                    `=out-interface-list=${groupName}`,
+
+                ])
+            })
+
+            .catch((err) =>{
+                logger.info(`Error routeros${err}`)
+            })
 
 })
+
+}
+
+module.exports = {
+    newVLAN,
+ }
