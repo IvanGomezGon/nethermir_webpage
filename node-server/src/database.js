ad1f24549cc67abc951f4d2cb45841825fd24adc
ivanG <ivangg02@gmail.com>
2023-08-15 17:33:42 +0200

    include generateRouterOSConfig changed

--- node-server/src/database.js
+++ node-server/src/database.js
@@ -5,7 +5,7 @@ var mysql = require('mysql2');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 const cloneMachine = require(path.resolve(__dirname, 'proxmox.js'))
 const emailManager = require(path.resolve(__dirname, 'emails.js'))
-const {routeros} = require(path.resolve(__dirname, 'routeros.js'))
+const {generateRouterOSConfig} = require(path.resolve(__dirname, 'routeros.js'))
 var logger = require(path.resolve(__dirname, 'logger.js'))
 
 
@@ -118,7 +118,7 @@ const authenticate = async(req, res) => {
                             portUDP = 65434 + vlanId
                             logger.info(`Before big logger`)
                             logger.info(`ROUTEROS: ${user}, ${wgRouterPrivateKey}, ${wgGroupPublicKey}, ${portUDP}, ${process.env.ROUTEROS_TO_PROXMOX_INTERFACE_NAME}, ${vlanId}`)
-                            routeros.generateRouterOSConfig(user, wgRouterPrivateKey, wgGroupPublicKey, portUDP, process.env.ROUTEROS_TO_PROXMOX_INTERFACE_NAME, vlanId)
+                            generateRouterOSConfig(user, wgRouterPrivateKey, wgGroupPublicKey, portUDP, process.env.ROUTEROS_TO_PROXMOX_INTERFACE_NAME, vlanId)
                             resolve(user)}
                         else {
                             feedback_fetch("Clonning failed - Contact Professor", res)
