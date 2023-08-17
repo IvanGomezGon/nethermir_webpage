4339e4385c8780f6509c24e4814978ccba76ecc7
ivanG <ivangg02@gmail.com>
2023-08-17 15:35:08 +0200

    write file now async

--- node-server/src/emails.js
+++ node-server/src/emails.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 var logger = require(path.resolve(__dirname, "logger.js"));
 
 const sendEmail = async (sendTo, txt, attachements = [{}]) => {
-    logger.info("SendEmail started")
+    logger.info("SendEmail started");
     return new Promise(async (resolve, reject) => {
         const transporter = nodeMailer.createTransport({
             host: process.env.SMTP_SERVER,
@@ -33,7 +33,7 @@ const sendWarningMail = (user) => {
     emails.forEach((email) => sendEmail(email, emailText));
 };
 const sendPasswordEmail = async (emails, groupName, endpointPort, password, keyPairUser, keyPairRouter) => {
-    logger.info("sendPasswordEmail")
+    logger.info("sendPasswordEmail");
     emailText = `Hola, les credencials per entrar al panell de gestió de Nethermir son: <br>
                 Usuari: ${groupName} <br>
                 Contransenya: ${password} <br><br>
@@ -60,23 +60,27 @@ const sendPasswordEmail = async (emails, groupName, endpointPort, password, keyP
         },
         { filename: `${nameGroup}.txt`, path: wireguardTxtPath },
     ];
-    logger.info(`Attachements: ${attachements}`)
+    logger.info(`Attachements: ${attachements}`);
     const zip = new JSZip();
     zip.file(`${groupName}.conf`, wireguardTxt);
     const content = await zip.generateAsync({ type: "nodebuffer" });
-    logger.info(`Content: ${content}`)
-    fs.writeFile(`${groupName}.zip`, content);
-    logger.info(`.zip created, sending emails...`)
-    for (const email of emails) {
-        logger.info(`Email ${email}`)
-        await sendEmail(email, emailText, attachements);
-    }
-    logger.info("Sending finished, unlinking")
-    fs.unlink(`${groupName}.zip`, (err) => {
+    logger.info(`Content: ${content}`);
+    fs.writeFile(`${groupName}.zip`, content, async (err) => {
         if (err) {
-            logger.error(`Error eliminating zip ${err}`);
+            logger.error(`Error creating zip ${err}`);
         }
-        logger.info("Unlinking ended")
+        logger.info(`.zip created, sending emails...`);
+        for (const email of emails) {
+            logger.info(`Email ${email}`);
+            await sendEmail(email, emailText, attachements);
+        }
+        logger.info("Sending finished, unlinking");
+        fs.unlink(`${groupName}.zip`, (err) => {
+            if (err) {
+                logger.error(`Error eliminating zip ${err}`);
+            }
+            logger.info("Unlinking ended");
+        });
     });
 };
 
